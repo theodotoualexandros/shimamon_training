8854d8e4f420b17c20ac4eeb13a11a6b3b5e9bb3
Alexandros THEODOTOU(JP) <theodotou.alexandros@monstar-lab.com>
2018-05-17 09:50:04 +0900

    add i18n to rspec

--- spec/features/task_features_spec.rb
+++ spec/features/task_features_spec.rb
@@ -4,8 +4,8 @@ RSpec.feature "Task management", :type => :feature do
   scenario "User creates a new task" do
     visit "/tasks/new"
 
-    fill_in "Name", :with => "My Name"
-    click_button "Create Task"
+    fill_in I18n.t('tasks.name'), :with => "My Name"
+    click_button I18n.t 'helpers.submit.create'
 
     expect(page).to have_text("Task was successfully created.")
   end
