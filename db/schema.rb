cbbccfa4a2072a5868aaf7575c7aba2ede579645
Alexandros THEODOTOU(JP) <theodotou.alexandros@monstar-lab.com>
2018-05-17 17:18:59 +0900

    added deadline field to tasks

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_05_17_034026) do
+ActiveRecord::Schema.define(version: 2018_05_17_080259) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 2018_05_17_034026) do
     t.string "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.date "deadline"
   end
 
 end
